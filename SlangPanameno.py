a1dbe2d8c50bf244aa1acb09653fdfacda1dc48e
Elmer Gonzalez <elmer.gonzalez@uip.edu.pa>
2023-07-18 23:01:37 -0500

    All features done, first complet revision

--- SlangPanameno.py
+++ SlangPanameno.py
@@ -1,7 +1,14 @@
-class SlangPanameno:
+from sqlalchemy import Column, String, Integer
+from base import Base
+
+
+class SlangPanameno(Base):
+    __tablename__ = "slangs"
+
+    id = Column(Integer(), primary_key=True)
+    word = Column(String(50), nullable=False, unique=True)
+    meaning = Column(String(50), nullable=False)
+
     def __init__(self, word, meaning):
-        self.id = None
         self.word = word
         self.meaning = meaning
-
-#Test comment
